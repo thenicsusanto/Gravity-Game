186f9a186d9ed6e8668591820416e94960a45a17
Nico Susanto <nicnad7788@gmail.com>
2022-06-28 23:08:52 -0700

    Somewhat got attacking fixed now just random

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
 	private float nextDashTime = 0;
 	public float dashCooldown = 2f;
 	bool isDashing = false;
+	bool isAttacking = false;
+	public bool canMove = true;
 
 	private Rigidbody rb;
 	private Transform playerModel;
@@ -24,15 +26,31 @@ public class PlayerController : MonoBehaviour
 	{
 		playerModel = transform.GetChild(0).transform;
 		rb = GetComponent<Rigidbody>();
-		anim = gameObject.transform.GetChild(0).GetComponent<Animator>();
+		//anim = gameObject.transform.GetChild(0).GetComponent<Animator>();
 		trailRenderer = GetComponent<TrailRenderer>();
 	}
 
 	void Update()
 	{
 		moveDirection = new Vector3(joystick.Horizontal, 0, joystick.Vertical).normalized;
-		RotateForward();
-		if(Time.time > nextDashTime)
+		if(moveDirection != Vector3.zero && isAttacking == false && !anim.GetCurrentAnimatorStateInfo(0).IsTag("SwordAttack") && !anim.IsInTransition(0) == true)
+		{
+			canMove = true;
+			RotateForward();
+			anim.SetBool("isRunning", true);
+		} else if(!anim.IsInTransition(0) == true)
+        {
+			canMove = false;
+			anim.SetBool("isRunning", false);
+        }
+		
+
+		if (Input.GetMouseButtonDown(1) && isAttacking == false && !anim.IsInTransition(0) == true)
+		{
+			PlayWrapper();
+		}
+
+		if (Time.time > nextDashTime)
         {
 			if (Input.GetKeyDown(KeyCode.E))
 			{
@@ -43,6 +61,7 @@ public class PlayerController : MonoBehaviour
 				StartCoroutine(StopDashing());
             }
 		}
+
 		
 	}
 
@@ -52,20 +71,15 @@ public class PlayerController : MonoBehaviour
 		// calculate angle and rotation
 		float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
 		Quaternion targetRotation = Quaternion.AngleAxis(targetAngle, Vector3.up); //sets rotation to target angle degrees around y axis
-		// only update rotation if direction greater than zero
-		if (Vector3.Magnitude(dir) > 0.1f)
-		{
-			playerModel.localRotation = targetRotation;
-			anim.SetBool("isRunning", true);
-		} else
-        {
-			anim.SetBool("isRunning", false);
-        }
+		playerModel.localRotation = targetRotation;
 	}
 
     void FixedUpdate()
 	{
-		rb.MovePosition(rb.position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+		if(canMove == true)
+        {
+			rb.MovePosition(rb.position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+		}
 		if(isDashing)
         {
 			commitDash();
@@ -85,6 +99,23 @@ public class PlayerController : MonoBehaviour
 		yield return new WaitForSeconds(dashCooldown);
 		trailRenderer.emitting = false;
 		isDashing = false;
+    }
+
+	public IEnumerator PlayRandomAttack()
+	{
+		isAttacking = true;
+		rb.velocity = Vector3.zero;
+		anim.SetInteger("AttackIndex", UnityEngine.Random.Range(0, 4));
+		anim.SetTrigger("Attack");
+		Debug.Log("Attacking");
+		yield return new WaitForSeconds(.8f);
+		isAttacking = false;
+
+	}
+
+	public void PlayWrapper()
+    {
+		StartCoroutine(PlayRandomAttack());
     }
 }
 			
